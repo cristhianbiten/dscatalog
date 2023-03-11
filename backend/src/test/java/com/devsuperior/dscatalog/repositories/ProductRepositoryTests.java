f12a2df54901aeb3da5c9f7c8b38fa4f9f68e39d
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-03-10 22:04:45 -0300

    Delele Product Test

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -25,19 +25,19 @@ public class ProductRepositoryTests {
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
-		nonExistingId = 1000L;
+		nonExistingId = 2000L;
 		countTotalProducts = 25L;
 	}
 
 	@Test
 	public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
-		
+
 		Product product = Factory.createProduct();
-		
+
 		product.setId(null);
-		
+
 		product = repository.save(product);
-		
+
 		Assertions.assertNotNull(product.getId());
 		Assertions.assertEquals(countTotalProducts + 1, product.getId());
 
@@ -60,5 +60,23 @@ public class ProductRepositoryTests {
 
 			repository.deleteById(nonExistingId);
 		});
+	}
+
+	@Test
+	public void findByIdShoulEmptyOptionalWhenIdDoesNotExist() {
+
+		Optional<Product> result = repository.findById(nonExistingId);
+
+		Assertions.assertTrue(result.isEmpty());
+
+	}
+
+	@Test
+	public void findByIdShouldFindObjectWhenIdExists() {
+
+		Optional<Product> result = repository.findById(existingId);
+
+		Assertions.assertTrue(result.isPresent());
+
 	}
 }
---
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -0,0 +1,72 @@
+package com.devsuperior.dscatalog.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.expections.DatabaseException;
+import com.devsuperior.dscatalog.services.expections.ResourceNotFoundException;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+	@InjectMocks
+	private ProductService service;
+
+	@Mock
+	private ProductRepository repository;
+
+	private Long existingId;
+	private Long nonExistingId;
+	private Long dependentId;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 2000L;
+		dependentId = 4L;
+
+		Mockito.doNothing().when(repository).deleteById(existingId);
+		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+
+	}
+
+	@Test
+	public void deleteShouldDoNothingWhenIdExists() {
+
+		Assertions.assertDoesNotThrow(() -> {
+			service.delete(existingId);
+		});
+
+		Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+	}
+
+	@Test
+	public void deleteShouldThrowsResourceNotFoundExceptionnWhenIdDoesNotExists() {
+
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+
+		Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowsDatabaseExceptionWhenDependentId() {
+
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			service.delete(dependentId);
+		});
+
+		Mockito.verify(repository, Mockito.times(1)).deleteById(dependentId);
+	}
+}
